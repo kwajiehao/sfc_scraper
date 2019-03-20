3666e40975cab13d0a490089b6030e68a2d7b137
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-03-20 17:29:23 +0800

    store results as tuples in list

--- sfc.py
+++ sfc.py
@@ -15,7 +15,8 @@ from selenium.webdriver.common.keys import Keys
 # http://www.marinamele.com/selenium-tutorial-web-scraping-with-selenium-and-python 
 
 chrome_driver = '/Users/kwa/Documents/Code/Registered Licensees/chromedriver'
-alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+letters = list(range(1,27)) # since there are 26 letters
+table_header = ['CE Reference', 'Name', 'Chinese Name', 'Main Business Address', 'Active License Record']
 
 def init_driver(wait_time, driver_path=chrome_driver):
 	"""function to initialize a chrome driver with a specified wait time to keep
@@ -28,7 +29,7 @@ def init_driver(wait_time, driver_path=chrome_driver):
 # note that a type 1 license has a button id of 1019, and each subsequent type is an 
 # increment of 1. Therefore, we can include what type license as an input
 
-def sfc_lookup(driver, type, letter_no):
+def sfc_lookup(driver, type):
 	"""lookup the license type on the SFC website"""
 	driver.get('https://www.sfc.hk/publicregWeb/searchByRa?locale=en')
 	try:
@@ -39,15 +40,6 @@ def sfc_lookup(driver, type, letter_no):
 		# select corporation type
 		biz_button = driver.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="roleTypeCorporation-inputEl"]')))
 		biz_button.click()
-		
-		# choose letter to search
-		dropdown_button = driver.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ext-gen1069"]')))
-		dropdown_button.click()
-		driver.find_element_by_xpath('//*[@id="boundlist-1064-listEl"]/ul/li[' + str(letter_no) + ']').click()
-
-		# click the search button
-		search_button = driver.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="button-1011-btnInnerEl"]')))
-		search_button.click()
 
 	except Exception as e:
 		error_message = str(e)
@@ -55,6 +47,70 @@ def sfc_lookup(driver, type, letter_no):
 		write_logs(driver, error_message, stack_trace)
 		# driver.close()
 		# exit("Exception occurred")
+
+def sfc_per_letter(driver, letter_no):
+	"""Collect all the pages of data for a given letter"""
+	# choose letter to search
+	dropdown_button = driver.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ext-gen1069"]')))
+	dropdown_button.click()
+	driver.find_element_by_xpath('//*[@id="boundlist-1064-listEl"]/ul/li[' + str(letter_no) + ']').click()
+
+	# click the search button
+	search_button = driver.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="button-1011-btnInnerEl"]')))
+	search_button.click()
+
+	# check the number of pages and items
+	pages = driver.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tbtext-1053"]')))
+	items = driver.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tbtext-1060"]')))
+	
+	# we need an explicit wait since the value of the element changes as the webpage loads
+	time.sleep(2.5) 
+	num_pages = int(pages.get_attribute("innerHTML").split(' ')[1])
+	num_items = int(items.get_attribute("innerHTML").split(' - ')[1].split(' ')[2])
+	print(num_pages)
+	return num_pages, num_items
+
+def sfc_all_pages_per_letter(driver, page_no, item_lim):
+	"""Scrape all data on the page"""
+	
+	# input page number into box
+	box = driver.find_element_by_xpath('//*[@id="numberfield-1052-inputEl"]')
+	box.clear()
+	box.send_keys(page_no)
+	box.send_keys(Keys.ENTER)
+
+	# explicit wait for page to load
+	time.sleep(2.5)
+
+	# count the total number of items on the page
+	total_items = item_lim - (20 * (page_no - 1))
+	if total_items > 20:
+		num_objects = 20
+	else:
+		num_objects = total_items
+
+	# store results
+	results = [None] * num_objects
+
+	# find table by xpath
+	table1 = '//*[@id="gridview-1046"]/table/tbody/tr['
+	table2 = ']/td['
+	table3 = ']/div'
+
+	# first row number is 2 since row number 1 is header
+	# store results in list as tuples
+	for row in range(2, num_objects+2):
+		field1 = driver.find_element_by_xpath(table1 + str(row) + table2 + str(1) + table3).get_attribute("innerHTML")
+		field2 = driver.find_element_by_xpath(table1 + str(row) + table2 + str(2) + table3 + '/a').get_attribute("innerHTML")
+		field3 = driver.find_element_by_xpath(table1 + str(row) + table2 + str(3) + table3).get_attribute("innerHTML")
+		field4 = driver.find_element_by_xpath(table1 + str(row) + table2 + str(6) + table3).get_attribute("innerHTML")
+		field5 = driver.find_element_by_xpath(table1 + str(row) + table2 + str(7) + table3).get_attribute("innerHTML")
+		results[row-2] = (field1, field2, field3, field4, field5)
+	
+	# print(results[0])
+	return results
+
+	
 
 def write_logs(driver, error_message, stack_trace):
 	"""function to write logs"""
@@ -66,7 +122,13 @@ def write_logs(driver, error_message, stack_trace):
 		outfile.write(error_message + '\n' + stack_trace)
 
 if __name__ == '__main__':
+	results = []
 	driver = init_driver(10000)
-	sfc_lookup(driver, 9, 3)
+	sfc_lookup(driver, 9)
+	for letter in letters:
+		page_lim, item_lim = sfc_per_letter(driver, letter)
+		for page in list(range(1, page_lim+1)):
+			results += sfc_all_pages_per_letter(driver, page, item_lim)
+
 
 
